72e450fa859da2470dde78c15cc4172b7e02c42a
Grey-What <greywaht2002@gmail.com>
2024-08-10 12:46:26 +0000

    function that safely divides

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,17 @@
+-- script that creates a function SafeDiv that divides (and returns)
+-- the first by the second number or returns 0 if the second number is equal to 0.
+DELIMITER //
+
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT
+DETERMINISTIC
+BEGIN
+	DECLARE ans FLOAT DEFAULT 0;
+
+	IF b != 0 THEN
+	   SET ans = a / b;
+	END IF;
+	RETURN ans;
+END;
+//
+DELIMITER ;

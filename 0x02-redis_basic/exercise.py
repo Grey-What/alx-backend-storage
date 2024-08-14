ea0083db6ef392fb8e3c16f692adda1a73b12b20
Grey-What <greywaht2002@gmail.com>
2024-08-14 14:24:30 +0000

    added a method to convert returned data + updated way of getting unique key

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,8 +2,8 @@
 """contains a class with a constructor and method"""
 
 import redis
-from typing import Union
-from uuid import uuid4
+from typing import Union, Callable
+import uuid
 
 
 class Cache:
@@ -24,7 +24,24 @@ class Cache:
 
         Return: key generated
         """
-        random_key = str(uuid4())
+        random_key = str(uuid())
         self._redis.set(random_key, data)
 
         return random_key
+
+    def get(self, key: str,
+            fn: Callable = None) -> Union[str, bytes, float, int]:
+        """retrieves a value from redis storage based on key"""
+        data = self._redis.get(key)
+
+        if fn is not None:
+            return fn(data)
+
+        return data
+
+    def get_str(self, key: str) -> str:
+        """retireves a string from mRedis storage"""
+        return self.get(key, lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        return self.get(key, lambda x: int(x))

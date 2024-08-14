54c79689e07b2f001ac15179fec8a2051f1c860a
Grey-What <greywaht2002@gmail.com>
2024-08-14 13:58:51 +0000

    set asynchronous to true

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -12,7 +12,7 @@ class Cache:
     def __init__(self) -> None:
         """constructor of class"""
         self._redis = redis.Redis()
-        self._redis.flushdb()
+        self._redis.flushdb(True)
 
     def store(self, data: Union[int, float, str, bytes]) -> str:
         """
